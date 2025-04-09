ca355fc932c3acf2b67a8b28c41312cfa9948c67
hamnerben <hamnerben@gmail.com>
2025-04-09 14:25:59 -0600

    added plate_number and id to plate_info

--- src/utils.py
+++ src/utils.py
@@ -10,13 +10,20 @@ def get_license_plate_info_list(train=True):
     next(reader)  # Skip the header row if it exists
 
     for row in reader:
+      # create the plate_info object
+      id = row[0]
       plate_number = row[1] 
-      plate_info = parse_license_plate(plate_number)
+      plate_info = parse_license_plate(id, plate_number)
       if(train):
         price = row[3]
         plate_info.price = price
+      # append to the list
       plate_info_list.append(plate_info)
   
   return plate_info_list
 
 
+if __name__ == "__main__":
+  plate_info_list = get_license_plate_info_list(train=True)
+  for plate_info in plate_info_list:
+    print(f"Plate Number: {plate_info.plate_number}, Region: {plate_info.region_name}, Price: {plate_info.price}")
