a5d57cec4726d2f3c6d915e64f549da38f4ec38d
Dalton Hansen <nesnahdl@gmail.com>
2025-04-09 13:40:25 -0600

    read in data

--- src/ben.py
+++ src/ben.py
@@ -2,17 +2,17 @@ from parse_plate import *
 import csv
 # Open the train CSV file
 
-plate_info_
-with open('train.csv', mode='r', encoding='utf-8') as file:
+# plate_info_
+with open('data/train.csv', mode='r', encoding='utf-8') as file:
   reader = csv.reader(file)
   next(reader)  # Skip the header row if it exists
 
   for row in reader:
     plate_number = row[1] 
-    plate_info = parse_plate(plate_number)
+    plate_info = parse_license_plate(plate_number)
     
-    if plate_info['valid']:
-      print(f"Plate Number: {plate_number}, Region: {parsed_data['region_name']}")
+    if plate_info.valid:
+      print(f"Plate Number: {plate_number}, Region: {plate_info.region_name}")
 # read the train csv in
 # for each line in the csv
     # parse the plate number
