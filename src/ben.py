9740617fc8fbc5ca6ea66486b43efe5e58cee4ef
Julie Olson <a02363064@usu.edu>
2025-04-10 17:31:15 -0600

    graphs

--- src/ben.py
+++ src/ben.py
@@ -8,16 +8,16 @@ with open('src/data/train.csv', mode='r', encoding='utf-8') as file:
 
     # First pass: Calculate the total price and count the rows
     totals_price = 0
-    min_price = 1000000000000
+    # min_price = 1000000000000
     row_count = 0
     for row in reader:
         price = float(row[3])  # Convert price to float
-        # totals_price += price
-        if price < min_price:
-            min_price = price
+        totals_price += price
+        # if price < min_price:
+        #     min_price = price
         row_count += 1
 
-    # mean_price = totals_price / row_count  # Calculate mean price
+    mean_price = totals_price / row_count  # Calculate mean price
 
 # Open the test CSV file to get the IDs
 with open('src/data/test.csv', mode='r', encoding='utf-8') as test_file:
@@ -25,10 +25,10 @@ with open('src/data/test.csv', mode='r', encoding='utf-8') as test_file:
     test_header = next(test_reader)  # Skip the header row if it exists
 
     # Second pass: Write the mean price for every ID from test.csv to submission.csv
-    with open('src/data/min_baseline.csv', mode='w', encoding='utf-8', newline='') as submission_file:
+    with open('src/data/mean_baseline.csv', mode='w', encoding='utf-8', newline='') as submission_file:
         writer = csv.writer(submission_file)
         writer.writerow(['id', 'price'])  # Write the header row
 
         for test_row in test_reader:
             plate_id = test_row[0]  # Get the ID from the first column of test.csv
-            writer.writerow([plate_id, min_price])  # Write the mean price for each ID
+            writer.writerow([plate_id, mean_price])  # Write the mean price for each ID
